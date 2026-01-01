368a9dd7a38dc391f92f0b975a8e9dce3a460e9f
Abdul Hadi <abdul1729401@gmail.com>
2026-01-01 20:34:54 +0500

    project

---
+++ script.js
@@ -0,0 +1,27 @@
+// console.log(buttons)
+
+let display = document.getElementById("input");
+let buttons = document.querySelectorAll("button");
+
+let btnarr = Array.from(buttons);
+let string ="";
+btnarr.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+            if (e.target.innerHTML == "DEL") {
+                string = string.substring(0, string.length - 1)
+                display.value=string
+            }
+            else if (e.target.innerHTML == "AC") {
+                string = "";
+                display.value=string
+            }
+            else if(e.target.innerHTML=="="){
+                string=eval (string);
+                display.value=string;
+            }
+            else{
+                string +=e.target.innerHTML;
+                display.value=string;
+            }
+        })
+})
